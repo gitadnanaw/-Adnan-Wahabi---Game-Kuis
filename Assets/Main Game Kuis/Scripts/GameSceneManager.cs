0279fa085329eb4149534f291f94bd995ed46a0a
adnanaw_ <adnanaw1080@gmail.com>
2024-05-20 03:42:00 +0700

    init
    Build Game

--- Assets/Main Game Kuis/Scripts/GameSceneManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class GameSceneManager : MonoBehaviour
-{
-    public void BukaScene (string namaScene)
-    {
-        SceneManager.LoadScene (namaScene);
-    }
-}
